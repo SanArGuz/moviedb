70a1c82cbb08ef953da4b2906cbe7288bf77addb
sanarguz <santiago.arcilag@gmail.com>
2020-08-25 18:14:34 -0500

    Updates

---
+++ precache-manifest.a9eb164ab799c3d0a4c71897ad690a70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8a8b073e2d26dff226d910bd9214194",
+    "url": "/moviedb/index.html"
+  },
+  {
+    "revision": "f15da3085c9308c3e046",
+    "url": "/moviedb/static/css/main.9425a57c.chunk.css"
+  },
+  {
+    "revision": "76f7a7d88c4800f8032c",
+    "url": "/moviedb/static/js/2.ff559574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedb/static/js/2.ff559574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f15da3085c9308c3e046",
+    "url": "/moviedb/static/js/main.ce97cb70.chunk.js"
+  },
+  {
+    "revision": "fa7419bc2d2ca466b3f4",
+    "url": "/moviedb/static/js/runtime-main.0348c0bf.js"
+  }
+]);
